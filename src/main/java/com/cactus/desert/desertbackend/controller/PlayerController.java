7639bd2d52471eea273f5caf4f875e1196cb5caa
EvanLuo42 <ziyun.luo@icloud.com>
2022-05-02 19:46:05 +0800

    refactor: Removed nesting `If` expression

--- src/main/java/com/cactus/desert/desertbackend/controller/PlayerController.java
+++ src/main/java/com/cactus/desert/desertbackend/controller/PlayerController.java
@@ -1,16 +1,11 @@
 package com.cactus.desert.desertbackend.controller;
 
 import com.cactus.desert.desertbackend.Result;
-import com.cactus.desert.desertbackend.dto.PlayerInfo;
-import com.cactus.desert.desertbackend.dto.TokenDetail;
+import com.cactus.desert.desertbackend.dto.*;
 import com.cactus.desert.desertbackend.entity.Player;
-import com.cactus.desert.desertbackend.form.FieldError;
-import com.cactus.desert.desertbackend.form.GetPlayerByIdForm;
-import com.cactus.desert.desertbackend.form.LoginForm;
-import com.cactus.desert.desertbackend.form.RegisterForm;
+import com.cactus.desert.desertbackend.form.*;
 import com.cactus.desert.desertbackend.service.PlayerService;
-import com.cactus.desert.desertbackend.util.I18nUtil;
-import com.cactus.desert.desertbackend.util.TokenUtil;
+import com.cactus.desert.desertbackend.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -59,36 +54,37 @@ public class PlayerController {
         List<FieldError> formValidate = playerService.validateForm(form);
         Result result = new Result();
 
-        if (formValidate.isEmpty()) {
-            return playerService.getPlayerById(playerId)
-                    .map(info -> result.success(i18nUtil.getMessage("player.getByIdSuccess"), info))
-                    .orElse(result.notFound(i18nUtil.getMessage("player.notFound")));
+        if (!formValidate.isEmpty()) {
+            return result.formError(i18nUtil.getMessage("common.invalidForm"),
+                    formValidate);
         }
 
-        return result.formError(i18nUtil.getMessage("common.invalidForm"),
-                formValidate);
+        return playerService.getPlayerById(playerId)
+                .map(info -> result.success(i18nUtil.getMessage("player.getByIdSuccess"), info))
+                .orElse(result.notFound(i18nUtil.getMessage("player.notFound")));
     }
 
     @PostMapping(path = "/player/register")
     public ResponseEntity<Result> register(@RequestBody RegisterForm form) {
         List<FieldError> formValidate = playerService.validateForm(form);
         Result result = new Result();
-        if (formValidate.isEmpty()) {
-            Player player = new Player();
-            player.setPlayerName(form.getPlayerName());
-            player.setPlayerPassword(BCrypt.hashpw(form.getPlayerPassword(), BCrypt.gensalt()));
-            player.setPlayerEmail(form.getPlayerEmail());
-            player.setActive(true);
-            player.setAdmin(false);
-
-            if (!playerService.createPlayer(player)) {
-                return result.conflict(i18nUtil.getMessage("player.exist"));
-            }
-
-            return result.success(i18nUtil.getMessage("player.registerSuccess"),
-                    Optional.of(player).map(PlayerInfo::new).get());
+        if (!formValidate.isEmpty()) {
+            return result.formError(i18nUtil.getMessage("common.invalidForm"), formValidate);
         }
-        return result.formError(i18nUtil.getMessage("common.invalidForm"), formValidate);
+
+        Player player = new Player();
+        player.setPlayerName(form.getPlayerName());
+        player.setPlayerPassword(BCrypt.hashpw(form.getPlayerPassword(), BCrypt.gensalt()));
+        player.setPlayerEmail(form.getPlayerEmail());
+        player.setActive(true);
+        player.setAdmin(false);
+
+        if (!playerService.createPlayer(player)) {
+            return result.conflict(i18nUtil.getMessage("player.exist"));
+        }
+
+        return result.success(i18nUtil.getMessage("player.registerSuccess"),
+                Optional.of(player).map(PlayerInfo::new).get());
     }
 
     @PostMapping(path = "/player/login")
@@ -96,24 +92,24 @@ public class PlayerController {
         List<FieldError> formValidate = playerService.validateForm(form);
         Result result = new Result();
 
-        if (formValidate.isEmpty()) {
-            if (playerService.loginPlayer(form.getPlayerName(), form.getPlayerPassword())) {
-                Optional<PlayerInfo> playerInfo = playerService.getPlayerByName(form.getPlayerName());
-                if (playerInfo.map(PlayerInfo::isActive).orElse(false)) {
-                    TokenDetail tokenDetail = new TokenDetail();
-                    tokenDetail.setPlayerName(playerInfo.map(PlayerInfo::getPlayerName).orElse(null));
-                    tokenDetail.setAdmin(playerInfo.map(PlayerInfo::isAdmin).orElse(false));
-                    String token = tokenUtil.generateToken(tokenDetail);
+        if (!formValidate.isEmpty()) {
+            return result.formError(i18nUtil.getMessage("common.invalidForm"), formValidate);
+        }
 
-                    return result.success(i18nUtil.getMessage("player.loginSuccess"), token);
-                }
-
-                return result.unAuthorized(i18nUtil.getMessage("player.banned"));
-            }
-
+        if (!playerService.loginPlayer(form.getPlayerName(), form.getPlayerPassword())) {
             return result.unAuthorized(i18nUtil.getMessage("player.nameOrPasswordWrong"));
         }
 
-        return result.formError(i18nUtil.getMessage("common.invalidForm"), formValidate);
+        Optional<PlayerInfo> playerInfo = playerService.getPlayerByName(form.getPlayerName());
+
+        if (!playerInfo.map(PlayerInfo::isActive).orElse(false)) {
+            return result.unAuthorized(i18nUtil.getMessage("player.banned"));
+        }
+
+        TokenDetail tokenDetail = new TokenDetail();
+        tokenDetail.setPlayerName(playerInfo.map(PlayerInfo::getPlayerName).orElse(""));
+        tokenDetail.setAdmin(playerInfo.map(PlayerInfo::isAdmin).orElse(false));
+
+        return result.success(i18nUtil.getMessage("player.loginSuccess"), tokenUtil.generateToken(tokenDetail));
     }
 }
